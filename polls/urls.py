889fad5753cf8c623fc0545f1044297c378c01df
浜口昌武 <akimuhamaguchi@hamaguchimasatakenoMacBook-Pro.local>
2021-02-22 21:34:03 +0900

    URL

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
+urlpatterns = {
     path('', views.index, name='index'),
-]
+    path('show/<int:user>', views.show, name='show')
+}
--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. Fuck you man!!")
+
+
+def show(request, user):
+    return HttpResponse("あなたは{}番目の訪問者です".format(user))
